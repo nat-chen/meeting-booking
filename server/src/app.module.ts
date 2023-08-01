8d43cdeda4c5aa1fb548fef56755a444db18d0bb
Nat Chen <chen_natalie@outlook.com>
2023-08-01 15:56:27 +0800

    feat: create server project

---
+++ server/src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { join } from 'path';
+import { RedisModule } from './redis/redis.module';
+import { EmailModule } from './email/email.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'meeting_room_booking_system',
+      synchronize: true,
+      logging: true,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      },
+    }),
+    UserModule,
+    RedisModule,
+    EmailModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
